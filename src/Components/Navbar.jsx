281ebdd79d2d2becc20edda56ec03262e82ea425
Ananya Pal <ananya.pal@iemlabs.com>
2024-01-15 18:15:26 +0530

    1st

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div class="topnav">
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  );
+}
+
+export default Navbar;
