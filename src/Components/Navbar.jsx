d7118f94b66b2b1b77de781252bc336b976b6c8b
Ananya Pal <ananya.pal@iemlabs.com>
2024-01-16 16:29:49 +0530

    THIK HOISE :)

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,35 +1,53 @@
-import React, {useState} from 'react';
-import About from './About';
-import Review from './Review';
-import Contact from './Contact';
+import React, { useState } from "react";
+import About from "../Pages/About";
+import Review from "../Pages/Review";
+import Contact from "../Pages/Contact";
 import "./Navbar.css";
 
-import './adelia.ttf';
+import "./adelia.ttf";
 
 function Navbar(props) {
-  const [currentPage, setCurrentPage] = useState('');
+  const [currentPage, setCurrentPage] = useState("");
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
 
-  const renderPage = () => (
-    currentPage === 'home' ? <Home /> :
-    currentPage === 'about' ? <About /> :
-    currentPage === 'review' ? <Review /> :
-    currentPage === 'contact' ? <Contact /> :
-    null
-  );
+  const renderPage = () =>
+    // currentPage === 'home' ? <Home /> :
+    currentPage === "about" ? (
+      <About />
+    ) : currentPage === "review" ? (
+      <Review />
+    ) : currentPage === "contact" ? (
+      <Contact />
+    ) : null;
 
   return (
     <div className="topnav">
       <nav>
-      <h1>Ananya Pal</h1>  
+        <h1>Ananya Pal</h1>
         <ul>
-          <li><a href="#home" onClick={() => handlePageChange('home')}>{props.home}</a></li>
-          <li><a href="#about" onClick={() => handlePageChange('about')}>{props.about}</a></li>
-          <li><a href="#review" onClick={() => handlePageChange('review')}>{props.review}</a></li>
-          <li><a href="#contact" onClick={() => handlePageChange('contact')}>{props.contact}</a></li>     
+          {/* <li>
+            <a href="#home" onClick={() => handlePageChange("home")}>
+              {props.home}
+            </a>
+          </li> */}
+          <li>
+            <a href="#about" onClick={() => handlePageChange("about")}>
+              {props.about}
+            </a>
+          </li>
+          <li>
+            <a href="#review" onClick={() => handlePageChange("review")}>
+              {props.review}
+            </a>
+          </li>
+          <li>
+            <a href="#contact" onClick={() => handlePageChange("contact")}>
+              {props.contact}
+            </a>
+          </li>
         </ul>
       </nav>
 
